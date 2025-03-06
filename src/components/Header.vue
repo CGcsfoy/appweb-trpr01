620c3ea692de144b0f84b32fdddb06d319c607b2
CedricGourdes <2346909@csfoy.ca>
2025-03-06 17:42:42 -0500

    Ajout du workflow

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,8 +4,7 @@
 
 <template>
     <header class="bg-dark text-white position-relative">
-        <img src="https://cdn.media.amplience.net/i/k2/k2_2425_homepage-category-banner_ski?w=1600&fmt=webp&fmt.interlaced=true&qlt=75&protocol=https"
-            alt="Bannière de ski" class="img-fluid w-100" />
+        <img src="/src/assets/images/baniereSki.jpg" alt="Bannière de ski" class="img-fluid w-100" />
         <div class="position-absolute top-50 end-0 translate-middle-y text-end p-3 bg-dark bg-opacity-50">
             <h1 class="fw-bold">Gestionnaire de ski</h1>
             <p>Gérez facilement votre stock de skis.</p>
--- src/components/ProductExport.vue
+++ src/components/ProductExport.vue
@@ -25,9 +25,10 @@ const exportCSV = () => {
     ).join("\n");//Fait que chaque produit devient une ligne distincte.
 
     //Doc: https://medium.com/%40jeremy.w.barbara/how-to-download-a-csv-upon-button-click-in-javascript-e3ae1bae2178
-    const csvContent = csvHeader + csvRows;
+    //"\uFEFF" = gestion forcée des charactères spécaux (en plus de utf-8)
+    const csvContent = "\uFEFF" + csvHeader + csvRows;
     //Doc: https://stackoverflow.com/questions/56154046/downloading-blob-with-type-text-csv-strips-unicode-bom
-    const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
+    const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8" });
     const url = URL.createObjectURL(blob);
     const link = document.createElement("a");
 
